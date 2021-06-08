bb5de07c93eba600301b7eef40b2222467977c4a
Johannes Büttner <johannes@pop-os>
2021-06-08 15:26:15 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -n "$1" ]; then
+    mkdir -p $ANDROID_HOME/licenses
+    echo -e $1 >> $ANDROID_HOME/licenses/android-sdk-license
+    echo $"\nLicences accepted"
+fi
+
 echo -e "\n--> Running 'sh gradlew $2'\n"
 
-sh gradlew $1
+sh gradlew $2
